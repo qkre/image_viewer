6a466ad7f3484ec80b437195dd9a3f415f3eccc2
White-S-H <115938372+White-S-H@users.noreply.github.com>
2023-07-22 05:05:47 +0900

    Update untitled.py

--- untitled.py
+++ untitled.py
@@ -1,5 +1,6 @@
 import sys
 import cv2
+import numpy as np
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -30,6 +31,22 @@ class WindowClass(QMainWindow, form_class):
         self.pushButton3.setText("Convert to HSV")
         self.pushButton3.clicked.connect(self.cvt_hsv)
 
+        # 탭 2
+        self.pushButton5.setText("Convert to Lab Image")
+        self.pushButton5.clicked.connect(self.cvt_lab_image)
+
+        self.pushButton6.setText("Convert to Invert Image")
+        self.pushButton6.clicked.connect(self.cvt_invert_image)
+        
+        self.pushButton7.setText("Convert to GaussianBlur Image")
+        self.pushButton7.clicked.connect(self.cvt_GaussianBlur_image)
+
+        self.pushButton8.setText("Convert to Sharpen Image")
+        self.pushButton8.clicked.connect(self.cvt_sharpen_image)
+
+        
+        
+        
         # 슬라이더 1
         self.groupBox_12.setTitle("Image Size")
         self.horizontalSlider1.setMinimum(50)
@@ -160,6 +177,98 @@ class WindowClass(QMainWindow, form_class):
         scaled_image = cv2.resize(image, (new_width, new_height))
 
         return scaled_image
+    
+    def cvt_lab_image(self):
+        image = self.original_image
+
+        # 이미지 변환 상태 여부에 따라 처리
+        if self.is_image_cvt:
+            # 이미 변환된 상태일 때는 원본 이미지를 표시
+            self.display_image(self.original_image)
+            self.pushButton5.setText("Convert to Lab Image")  # 버튼 텍스트 변경
+            self.is_image_cvt = False
+        else:
+            # 아직 변환되지 않은 상태일 때는 이미지 밝기 강조, 녹색을 마젠타 색, 파랑색을 노랑색으로 반전 처리로 변환된 이미지를 표시
+            lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+            height, width, channel =  lab_image.shape
+            bytes_per_line = channel * width
+            q_image = QImage(
+            lab_image.data, width, height, bytes_per_line, QImage.Format_RGB888)
+            pixmap = QPixmap.fromImage(q_image)
+            self.image_label.setPixmap(pixmap)
+            self.pushButton5.setText("Restore Original")  # 버튼 텍스트 변경
+            self.is_image_cvt = True
+    
+    def cvt_invert_image(self):
+        image = self.original_image
+
+        # 이미지 변환 상태 여부에 따라 처리
+        if self.is_image_cvt:
+            # 이미 변환된 상태일 때는 원본 이미지를 표시
+            self.display_image(self.original_image)
+            self.pushButton6.setText("Convert to Invert Image")  # 버튼 텍스트 변경
+            self.is_image_cvt = False
+        else:
+            # 이미지 전체 색상 반전로 변환된 이미지를 표시
+            invert_image = cv2.bitwise_not(image) 
+            # 변환된 이미지를 PyQt의 라벨에 표시
+            height, width, channel = invert_image.shape
+            bytes_per_line = channel * width
+            q_image = QImage(
+                invert_image.data, width, height, bytes_per_line, QImage.Format_RGB888
+            )
+            pixmap = QPixmap.fromImage(q_image)
+            self.image_label.setPixmap(pixmap)  
+            self.pushButton6.setText("Restore Original")  # 버튼 텍스트 변경
+            self.is_image_cvt = True
+
+    def cvt_GaussianBlur_image(self):
+        image = self.original_image
+
+        # 이미지 변환 상태 여부에 따라 처리
+        if self.is_image_cvt:
+            # 이미 변환된 상태일 때는 원본 이미지를 표시
+            self.display_image(self.original_image)
+            self.pushButton7.setText("Convert to GaussianBlur Image")  # 버튼 텍스트 변경
+            self.is_image_cvt = False
+        else:
+            # 아직 변환되지 않은 상태일 때는 가우시안 블러로 변환된 이미지를 표시
+            blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+            height, width, channel = blurred_image.shape
+            bytes_per_line = channel * width
+            q_image = QImage(
+                blurred_image.data, width, height, bytes_per_line, QImage.Format_RGB888
+            )
+            pixmap = QPixmap.fromImage(q_image)
+            self.image_label.setPixmap(pixmap)
+            self.pushButton7.setText("Restore Original")  # 버튼 텍스트 변경
+            self.is_image_cvt = True
+
+    def cvt_sharpen_image(self):
+        image = self.original_image
+
+        # 이미지 변환 상태 여부에 따라 처리
+        if self.is_image_cvt:
+            # 이미 변환된 상태일 때는 원본 이미지를 표시
+            self.display_image(self.original_image)
+            self.pushButton8.setText("Convert to Sharpen Image")  # 버튼 텍스트 변경
+            self.is_image_cvt = False
+        else:
+            # 아직 변환되지 않은 상태일 때는 샤프닝 필터를 적용한 이미지를 표시
+            sharpen_kernel = np.array([[-1, -1, -1],
+                                       [-1,  9, -1],
+                                       [-1, -1, -1]])
+            sharpened_image = cv2.filter2D(image, -1, sharpen_kernel)
+
+            # 이미지 라벨에 조정된 이미지 표시
+            self.display_image(sharpened_image)
+            self.pushButton8.setText("Restore Original")  # 버튼 텍스트 변경
+            self.is_image_cvt = True
+    
+    
+        
+    
+    
 
 
 if __name__ == "__main__":
